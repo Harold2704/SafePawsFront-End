80cdb0beed16d0fb1d83dc79751a58ade493de2b
Chris210721 <138145537+Chris210721@users.noreply.github.com>
2025-07-11 18:49:08 -0500

    Create tema.ts

---
+++ datas/tema.ts
@@ -0,0 +1,4 @@
+const btn = document.getElementById("cambiarTema")!;
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
